2f1844463f77a4d51bf4e8d5d7f4e48007025763
daesung <daesung@mediaweb.co.kr>
2026-05-08 17:16:57 +0900

    ai_test

--- lyricvideo/lyric-video-remotion/src/mediaManifest.ts
+++
@@ -1,9 +0,0 @@
-export const mediaManifest = {
-  audioSrc: "/media/너는 어쩌다.wav",
-  lyricKind: "lrc",
-  lyricSrc: "/media/628ebff5-6de8-496d-a3b6-b214d4c226ef-lyrics-suno-가사-다운로더.lrc",
-  backgroundKind: "image",
-  backgroundSrc: "/media/bgimage.png",
-  title: null,
-  artist: null,
-} as const;
